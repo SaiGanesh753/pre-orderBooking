1b1bcf09c5f82f56ac48758f81e2eccb1864d491
SaiGanesh753 <saiganeshmutyala@gmail.com>
2023-12-13 20:02:28 +0530

    added models and udpated banner.

--- src/main/java/com/msg/preOrderBooking/restController/UserAuthenticationRestActions.java
+++ src/main/java/com/msg/preOrderBooking/restController/UserAuthenticationRestActions.java
@@ -1,4 +1,4 @@
-package restController;
+package com.msg.preOrderBooking.restController;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/main/java/com/msg/preOrderBooking/restController/model/RegistrationH.java
@@ -0,0 +1,50 @@
+package com.msg.preOrderBooking.restController.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationH {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "registrationH_id")
+    private Long registrationHId;
+    @Column
+    private Long registrationId;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String username;
+    @Column
+    private String contactEmail;
+    @Column
+    private String phoneNumber;
+    @Column
+    private Boolean verificationFlg;
+    @Column
+    private String passcode;
+    @Column
+    private Timestamp lastLoggedInDate;
+    @Column
+    private Boolean activeFlg;
+    @Column
+    private Boolean deleteFlg;
+    @Column
+    private Timestamp createdDt;
+    @Column
+    private String createdUser;
+    @Column
+    private Timestamp modifiedDt;
+    @Column
+    private String modifiedUser;
+}
